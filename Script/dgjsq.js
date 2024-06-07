612bb8c8ccb9e21dd814662071b4d393c05e0b1f
MoonYuki <49096663+MoonYuki@users.noreply.github.com>
2024-06-07 17:12:19 +0800

    Update dgjsq.js

--- Script/dgjsq.js
+++ Script/dgjsq.js
@@ -1,14 +1,13 @@
-const anni = {};
-const anni1 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const jsondata = JSON.parse(typeof $response != "undefined" && $response.body || null);
 
 if (typeof $response == "undefined") {
   delete $request.headers["x-revenuecat-etag"];
   delete $request.headers["X-RevenueCat-ETag"];
-  anni.headers = $request.headers;
-} else if (anni1 && anni1.subscriber) {
-  anni1.subscriber.subscriptions = anni1.subscriber.subscriptions || {};
-  anni1.subscriber.entitlements = anni1.subscriber.entitlements || {};
-  anni1.subscriber.non_subscriptions = {
+  jsondata.headers = $request.headers;
+} else if (jsondata && jsondata.subscriber) {
+  jsondata.subscriber.subscriptions = jsondata.subscriber.subscriptions || {};
+  jsondata.subscriber.entitlements = jsondata.subscriber.entitlements || {};
+  jsondata.subscriber.non_subscriptions = {
     "electricalcalculations.pro.lifetime": [
       {
         "id": "0123456789",
@@ -20,22 +19,22 @@ if (typeof $response == "undefined") {
       }
     ]
   };
-  anni1.subscriber.original_application_version = "112";
-  anni1.subscriber.other_purchases = {
+  jsondata.subscriber.original_application_version = "112";
+  jsondata.subscriber.other_purchases = {
     "electricalcalculations.pro.lifetime": {
       "purchase_date": "2023-12-31T16:00:00Z"
     }
   };
-  anni1.subscriber.management_url = "https://apps.apple.com/account/subscriptions";
-  anni1.subscriber.entitlements["pro"] = {
+  jsondata.subscriber.management_url = "https://apps.apple.com/account/subscriptions";
+  jsondata.subscriber.entitlements["pro"] = {
     "grace_period_expires_date": null,
     "purchase_date": "2023-12-31T16:00:00Z",
     "product_identifier": "electricalcalculations.pro.lifetime",
     "expires_date": null
   };
-  anni1.subscriber.original_purchase_date = "2023-12-31T16:00:00Z";
+  jsondata.subscriber.original_purchase_date = "2023-12-31T16:00:00Z";
 
-  anni.body = JSON.stringify(anni1);
+  jsondata.body = JSON.stringify(jsondata);
 }
 
-$done(anni);
+$done(jsondata);
