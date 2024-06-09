991df50e805f9d4eee3a683f42719d969609dc04
MoonYuki <49096663+MoonYuki@users.noreply.github.com>
2024-06-09 20:52:05 +0800

    Update Sub-info.js

--- Script/Sub-info/Sub-info.js
+++ Script/Sub-info/Sub-info.js
@@ -2,8 +2,9 @@
  * 由@mieqq编写
  * 原脚本地址：https://raw.githubusercontent.com/mieqq/mieqq/master/sub_info_panel.js
  * 由@Rabbit-Spec修改
- * 更新日期：2022.08.24
- * 版本：1.5
+ * 由@MoonYuki优化
+ * 更新日期：2024.06.09
+ * 版本：1.6
 */
 
 let args = getArgs();
