ca94cee066df756480205e4d9223f0cb21b061e0
MoonYuki <49096663+MoonYuki@users.noreply.github.com>
2024-06-15 19:18:39 +0800

    Update dgjsq.js

--- Script/Dgjsq/dgjsq.js
+++ Script/Dgjsq/dgjsq.js
@@ -1,10 +1,13 @@
 const jsondata = JSON.parse(typeof $response != "undefined" && $response.body || null);
+console.log("start");
 
 if (typeof $response == "undefined") {
+  console.log("response undefined");
   delete $request.headers["x-revenuecat-etag"];
   delete $request.headers["X-RevenueCat-ETag"];
   jsondata.headers = $request.headers;
 } else if (jsondata && jsondata.subscriber) {
+  console.log("rewrite start");
   jsondata.subscriber.subscriptions = jsondata.subscriber.subscriptions || {};
   jsondata.subscriber.entitlements = jsondata.subscriber.entitlements || {};
   jsondata.subscriber.non_subscriptions = {
@@ -37,4 +40,5 @@ if (typeof $response == "undefined") {
   jsondata.body = JSON.stringify(jsondata);
 }
 
+console.log("script end");
 $done(jsondata);
