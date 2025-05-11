ac5c1b863f00338d6f67d230cc3abb31282864d7
MoonYuki <49096663+MoonYuki@users.noreply.github.com>
2025-05-11 20:16:09 +0800

    Delete Script/Colamanaga directory

--- Script/Colamanaga/colamanagaad.js
+++
@@ -1,8 +0,0 @@
-const data = $response.body;
-console.log('正则开始');
-  $done();
-data = data.replace(
-    /if\s*\(\s*typeof\s+__ad\s*===\s*"undefined"\s*\)[^;]*;\s*/g,
-    ''
-);
-$done({ body: data });
